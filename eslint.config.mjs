7ff8d63925288742e63266275a882b754aaecd69
Timothy Jones <timothy.l.jones@gmail.com>
2026-07-18 13:27:27 +1000

    chore: Resolve peer dependency warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,66 +1,77 @@
-import globals from "globals";
-import js from "@eslint/js";
-import vitest from "eslint-plugin-vitest";
-import eslintConfigPrettier from "eslint-config-prettier";
+import globals from 'globals';
+import js from '@eslint/js';
+import vitest from '@vitest/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 /**
  * @type {import("eslint").Linter.Config}
  */
 export default [
   {
-    "ignores": [".git/", ".github/", ".husky/", ".scannerwork/", ".vscode/", "coverage/", "node_modules/"],
-    "name": "Files to ignore"
+    ignores: [
+      '.git/',
+      '.github/',
+      '.husky/',
+      '.scannerwork/',
+      '.vscode/',
+      'coverage/',
+      'node_modules/',
+    ],
+    name: 'Files to ignore',
   },
   {
     ...eslintConfigPrettier,
-    "name": "Prettier"
+    name: 'Prettier',
   },
   {
     ...js.configs.recommended,
-    "files": ["**/*.{js,cjs,mjs}"],
-    "languageOptions": {
-      "ecmaVersion": 2023
+    files: ['**/*.{js,cjs,mjs}'],
+    languageOptions: {
+      ecmaVersion: 2023,
     },
-    "name": "JavaScript files",
-    "rules": {
+    name: 'JavaScript files',
+    rules: {
       ...js.configs.recommended.rules,
-      "no-var": "error",
-      "no-unused-vars": [
-        "error",
+      'no-var': 'error',
+      'no-unused-vars': [
+        'error',
         {
-          "argsIgnorePattern": "_.*"
-        }
-      ]
-    }
+          argsIgnorePattern: '_.*',
+        },
+      ],
+    },
   },
   {
-    "files": ["**/*.mjs"],
-    "languageOptions": {
-      "sourceType": "module"
+    files: ['**/*.mjs'],
+    languageOptions: {
+      sourceType: 'module',
     },
-    "name": "JavaScript modules"
+    name: 'JavaScript modules',
   },
   {
-    "files": ["**/*.{js,cjs,mjs}"],
-    "languageOptions": {
-      "globals": {
-        ...globals.node
-      }
+    files: ['**/*.{js,cjs,mjs}'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
     },
-    "name": "Node.js files"
+    name: 'Node.js files',
   },
   {
     ...vitest.configs.recommended,
-    "files": ["test/**/*{spec,test,integration-test}.{js,cjs,mjs}", "test/mocks/vitest-mocks.js"],
-    "languageOptions": {
-      "globals": {
-        ...vitest.environments.env.globals
-      }
+    files: [
+      'test/**/*{spec,test,integration-test}.{js,cjs,mjs}',
+      'test/mocks/vitest-mocks.js',
+    ],
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals,
+      },
     },
-    "name": "Test files",
-    "rules": {
+    name: 'Test files',
+    rules: {
       ...vitest.configs.recommended.rules,
-      "vitest/expect-expect": "off"
-    }
-  }
+      'vitest/expect-expect': 'off',
+    },
+  },
 ];
